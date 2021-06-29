45e564cafa16aea9c097222ac42252dc6c36b75b
Corynne Moody <cmoody@elevenfifty.org>
2021-06-29 16:57:12 -0400

    pushing challenge for students to access

---
+++ DOM.js
@@ -0,0 +1,42 @@
+// There are 2 challenges here, make sure you read the directions carefully
+// First thing you need to do is run 'npm install' in the terminal
+
+// Then you should be ready to start!
+
+//Challenge 1
+// step 1: In this column, display a photo of your choice in the img tag below
+// using DOM manipulation. Make sure to include alt text!</p>
+// step 2: Add some lorem ipsum below the photo, and some appropriate
+// styling in and around the container (ie. change the font, background
+// color, just add some of your own pizzaz!) 
+
+// *************CHALLENGE 1 CODE HERE****************
+
+
+
+
+// *************************************************
+
+
+
+
+
+
+
+
+//Challenge 2
+// step 1: In this column write fetch from this API endpoint 'http://ergast.com/api/f1/2020/drivers/hamilton/constructors/mercedes/circuits/bahrain/results.json'.
+// making sure to include the header 'method' with the value 'GET'. 
+// step 2: Display the
+// - locality
+// - country
+// - circutName
+//  from the "Circuit" Object
+// You may loop through and display this code via DOM manipulation
+// however you see fit!
+
+// *************CHALLENGE 2 CODE HERE****************
+
+
+// **************************************************
+
